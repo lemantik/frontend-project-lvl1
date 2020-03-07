68baff7912c1c1cf1b6005b5fcfc0b44ce9960f3
Anton <alemiasheuski@expediagroup.com>
2020-03-06 18:49:07 -0800

    fix: reolve problem with wrong file names and import

--- Makefile
+++ Makefile
@@ -5,4 +5,4 @@ start:
 	node bin/brain-games.js
 
 publish:
-	npm publish --dry-runnode
+	npm publish --dry-run
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import greetUserByName from '../src/index';
+import greetUserByName from '../src/index.js'
 console.log('Welcome to the Brain Games!');
 greetUserByName();
